f37ff62bb8cf6cd893aac786f28673a935dc93c9
Jakl <57708725+Jakllp@users.noreply.github.com>
2026-05-02 19:12:00 +0200

    Fix cursor things

--- cursor_handler.gd
+++ cursor_handler.gd
@@ -3,13 +3,15 @@ extends Node
 var time_since_last = 0.0
 var time_per_frame = 0.5
 var cur_frame = 0
-var max_frames = 3
+var max_frames = 1
 
 @onready var cursor :SpriteFrames = load("res://assets/cursors/cursor.tres")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_custom_mouse_cursor(cursor.get_frame_texture("arrow",0))
+	Input.set_custom_mouse_cursor(cursor.get_frame_texture("beam", 0), Input.CURSOR_IBEAM)
+	Input.set_custom_mouse_cursor(cursor.get_frame_texture("pointer", 0), Input.CURSOR_POINTING_HAND)
 
 
 func advance_frame(delta :float) -> void:
@@ -17,8 +19,14 @@ func advance_frame(delta :float) -> void:
 	if time_per_frame < time_since_last:
 		time_since_last = 0.0
 		cur_frame = (cur_frame + 1) % max_frames
+		print(cur_frame)
 		
 		# Switch the stuff
-		Input.set_custom_mouse_cursor(cursor.get_frame_texture("arrow", cur_frame))
-		Input.set_custom_mouse_cursor(cursor.get_frame_texture("beam", cur_frame), Input.CURSOR_IBEAM)
-		Input.set_custom_mouse_cursor(cursor.get_frame_texture("pointer", cur_frame), Input.CURSOR_POINTING_HAND)
+		match(Input.get_current_cursor_shape()):
+			Input.CURSOR_POINTING_HAND:
+				Input.set_custom_mouse_cursor(cursor.get_frame_texture("pointer", cur_frame), Input.CURSOR_POINTING_HAND)
+			Input.CURSOR_IBEAM:
+				Input.set_custom_mouse_cursor(cursor.get_frame_texture("beam", cur_frame), Input.CURSOR_IBEAM)
+			_:
+				Input.set_custom_mouse_cursor(cursor.get_frame_texture("arrow", cur_frame))
+		
