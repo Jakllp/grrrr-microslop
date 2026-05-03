9573a66abc1fb04ba5d603bec5eba4677475c04d
Jakl <57708725+Jakllp@users.noreply.github.com>
2026-05-03 12:23:12 +0200

    Task checking stub

--- resources/globals/task_manager.gd
+++ resources/globals/task_manager.gd
@@ -6,17 +6,22 @@ var tasks = {
 
 var tasks_to_evaluate :Array[Task] = []
 
-
 func check_trigger_task(time_string :String) -> void:
 	var task :Task = tasks.get(time_string)
 	if task != null and !tasks_to_evaluate.has(task):
 		print(task.task_subject + " " + task.task_sender + " " + task.task_content)
 		tasks_to_evaluate.append(task)
 
-func check_tasks() -> void:
-	for task in tasks_to_evaluate:
-		match task.task_goal:
-			SentenceTask:
-				pass
-			_:
-				pass
+func check_task(task :Task) -> bool:
+	var successful := false
+	match task.task_goal:
+		SentenceTask:
+			pass
+		_:
+			pass
+	if successful:
+		tasks_to_evaluate.erase(task)
+	return successful
+
+func check_end_of_day() -> bool:
+	return tasks_to_evaluate.is_empty()
