e816809c79e9761a410d88c19052b50867fe04e2
event <event@r148-116.hdm.hdm-stuttgart.de>
2026-05-03 21:48:15 +0200

    Merge and souuuunds amaaaaaaaaaaazing

--- resources/end_screens/game_over.gd
+++ resources/end_screens/game_over.gd
@@ -1,10 +1,14 @@
 extends Control
 
 @export var slop_bar :ProgressBar
+@export var player :AudioStreamPlayer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	slop_bar.value = GameData.slopmeter
+	if player != null:
+		player.play()
 
 func _on_restart_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://resources/login-screen.tscn");
