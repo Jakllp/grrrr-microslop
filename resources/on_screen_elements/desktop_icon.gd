e02430593f32394be5ae6ca41199b0f1ecd65241
Jakl <57708725+Jakllp@users.noreply.github.com>
2026-05-03 12:49:41 +0200

    Better desktop icons

--- resources/on_screen_elements/desktop_icon.gd
+++ resources/on_screen_elements/desktop_icon.gd
@@ -2,3 +2,7 @@ class_name DesktopIcon
 extends TextureButton
 
 @export var program :DesktopManager.PROGRAMS = DesktopManager.PROGRAMS.FILES
+@export var title := "Program"
+
+func _ready() -> void:
+	$Label.text = title
