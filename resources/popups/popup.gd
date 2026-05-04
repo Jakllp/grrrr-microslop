70683266391bef6078388d29ab9cc8eebcf308c6
Jakl <57708725+Jakllp@users.noreply.github.com>
2026-05-04 10:53:42 +0200

    Did #14

--- resources/popups/popup.gd
+++ resources/popups/popup.gd
@@ -4,12 +4,23 @@ extends Node
 @export var slop_minus := 5
 @export var player :AudioStreamPlayer
 
-# Called when the node enters the scene tree for the first time.
+@export var dec_button :Button
+@export var acc_button :Button
+@export var spawn_area :Panel
+
 func _ready() -> void:
 	if player != null:
 		player.play()
+	
+	# Button Placement
+	dec_button.position = _calulate_button_pos(spawn_area.position, spawn_area.size, dec_button)
+	acc_button.position = _calulate_button_pos(spawn_area.position, spawn_area.size,acc_button)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _calulate_button_pos(anchor :Vector2, size :Vector2, button :Button) -> Vector2:
+	var x_offset = randi_range(0, size.x-button.size.x)
+	var y_offset = randi_range(0, size.y-button.size.y)
+	return Vector2(anchor.x + x_offset, anchor.y + y_offset)
+
 func _process(delta: float) -> void:
 	pass
 
